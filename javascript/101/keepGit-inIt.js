8dbc8f5146bae6c9a8176d711b17f7477f9f0a92
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-07 09:36:40 -0400

    the GIT info we learned

---
+++ javascript/101/keepGit-inIt.js
@@ -0,0 +1,73 @@
+// //# careerdevs11e
+// Learning the basics of GIT
+
+
+// make sure you are in the directory that you want to start your repo in.
+
+
+// git init (creates an empty repo)
+
+
+// code README.md
+//      add some info to your README.md file and SAVE IT
+
+// git add -A
+// git commit -m "first commit"
+
+// go to github create the new directory 
+
+// enter repo name
+// DO NOT INITIATE A NEW README.md
+
+// MAKE SURE YOU SELECT THE SSH TAB AND NOT THE HTTPS
+
+
+// NOW you want to add existing repo to GitHub
+// GO TO GITHUB.COM  THEN CLICK THE PLUS SIGN OR THE NEW REPOSITORY button
+
+
+// select the first like of code
+//      insert into VS Code
+// select the second line of code
+//      insert into VS Code
+
+// NOW YOU SET UP BOTH REPO LOCATIONS and the remote ssh (GitHub)
+
+//  back in current directory on VS Code
+
+//  MAKE SURE YOU ADD A NEW WORKABLE BRANCH THAT ISNT MASTER
+//  type the following to make a new branch
+
+// git checkout -b "branchName"       will create that brance of of master
+
+//      create a javascript file  javascriptfilename.js        SAVE
+
+
+// DO ALL YOU CURRENT PROJECT UPDATES HERE save them in VS Code and then type
+// git add -A
+// git commit -m "what changes you made ex = made arrary or made loops"
+// then change the branch back to master with
+// git checkout master
+
+
+// MAKE SURE YOU ARE IN MASTER BRANCH WITH 
+//      git branch
+
+
+// git push -u origin master
+
+// git push --all
+
+
+
+// git checkout master        just to make sure you are in the right branch
+// git merge master  will add all the changes from the branches to the master branch
+
+// git push --all         to update github
+
+
+
+
+// these are the lines of code from github:
+// git remote add origin git@github.com:username/repo.git  this will be ssh 
+// git push -u origin master
