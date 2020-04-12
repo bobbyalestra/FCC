63d24ee13281ff22399a972a30f419fa5d0b5e0f
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-12 12:57:20 -0400

    used push splice indexof and pop to remove who logged in of the security app

--- javascript/101/cameronscode.js
+++ javascript/101/cameronscode.js
@@ -1,3 +1,7 @@
 let name = "Cameron"
+let age = 5
 
+for (i = 0; i < age; i++){
+if (i < age);
 console.log(` Happy Birthday ${name} !!!!! `)
+}
---
+++ javascript/2020-04/fcc/securityscannerapp.js
@@ -0,0 +1,63 @@
+
+
+
+let parkingGate = {
+  currentKeyFobScanned: "",
+  currentPatronNameScanned: "",
+  currentTimeKeyFobScanned: "",
+
+  keyFobScannedInPentagon: [],
+  patronNameScannedInParkingLot: [],
+  timePatronScannedInParkingLot: [],
+
+  keyFobScannedOutofPentagon: [],
+  patronNameScannedOutOfParkingLot: [],
+  timePatronScannedOutOfParkingLot: [],
+  
+  inScan: function() {
+    this.keyFobScannedInPentagon.push(this.currentKeyFobScanned);
+    this.patronNameScannedInParkingLot.push(this.currentPatronNameScanned);
+    this.timePatronScannedInParkingLot.push(this.currentTimeKeyFobScanned);
+    console.log(
+      `:::In The Parking Garage ${this.timePatronScannedInParkingLot}:::`
+    );
+    console.log(this.keyFobScannedInPentagon);
+    console.log(this.patronNameScannedInParkingLot);
+  },
+  outScan: function() {
+    if (this.keyFobScannedInPentagon == this.currentKeyFobScanned) {
+      
+      this.keyFobScannedOutofPentagon.push(this.keyFobScannedInPentagon.splice(this.keyFobScannedInPentagon.indexOf(this.currentKeyFobScanned), 1    ).pop()  );
+      
+      this.patronNameScannedOutOfParkingLot.push(this.patronNameScannedInParkingLot.splice(this.patronNameScannedInParkingLot.indexOf(this.patronNameScannedInParkingLot), 1    ).pop()  );
+
+
+      this.timePatronScannedOutOfParkingLot.push(this.timePatronScannedInParkingLot.splice(this.timePatronScannedInParkingLot.indexOf(this.currentTimeKeyFobScanned), 1    ).pop()  );
+
+    }
+    
+
+      console.log(
+        `:::Out Of The Parking Garage ${this.timePatronScannedOutOfParkingLot}:::`
+      );
+      console.log(this.keyFobScannedOutofPentagon);
+      console.log(this.patronNameScannedOutOfParkingLot);
+     if (this.keyFobScannedInPentagon !== this.currentKeyFobScanned) {
+      console.log(
+        `:::WARNING ${this.currentPatronNameScanned} DID NOT ENTER THE PARKING GARAGE!:::`
+      );
+    }
+  }
+};
+function keyFobId(id, name, inout, time) {
+  parkingGate.currentKeyFobScanned = id;
+  parkingGate.currentPatronNameScanned = name;
+  parkingGate.currentTimeKeyFobScanned = time;
+  if (inout == "in") {
+    parkingGate.inScan();
+  } else if (inout == "out") {
+    parkingGate.outScan();
+  }
+}
+keyFobId("12345", "will", "in", "08:35");
+keyFobId("12345", "will", "out", "09:15");
